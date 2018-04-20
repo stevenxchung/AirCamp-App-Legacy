34725fb39214268bce75c8f4642ca563f421e39c
Steven <stevenxchung@gmail.com>
2018-04-19 21:33:51 -0400

    Adding Deployed v8
    See readme.md for updates

---
+++ Deployed/v8/readme.md
@@ -0,0 +1,89 @@
+# Deployed YelpCamp Application v8
+
+The deployed YelpCamp application has migrated to another repository due to the increased volume of files in the [bootcamp repository](https://github.com/stevenxchung/Web-Developer-Bootcamp). 
+This YelpCamp application deployment series will be based on [YelpCamp v12 (deployed)](https://github.com/stevenxchung/Web-Developer-Bootcamp/tree/master/Section%2038%20-%20Deploying/v12Deployed). 
+For more details and documentation please visit the linked YelpCamp v12 (deployed) repository mentioned previously. This website uses Heroku and mLab cloud database for hosting, you can see the latest stable build of the YelpCamp application [here](https://stormy-beyond-16831.herokuapp.com/).
+
+## UI Improvements (v1 updates)
+In deployed v1 of this application we will be implementing the following improvements:
+* Refactoring the UI for Login and Sign Up pages
+* Update the nav-bar menu
+    * Convert .container fluid to regular .container
+    * Add conditional active class to menu list items
+    * Add collapsible hamburger menu
+    * Make site responsive for mobile
+* Fix registration flash message bug
+
+## Google Maps (v2 updates)
+In deployed v2 of this application we will be adding a location feature to our application, using the Google Maps API. The steps are as follows:
+* Sign up for a google developer account
+* Get Google Maps API key
+    * Restrict Google Maps API key
+* Enable Gecoding API
+* Get another key for Geocoding API
+    * Add to application as ENV variable
+* Add Google Maps scripts to your application
+* Display the campground location in show.ejs
+* Update campground model
+* Update new and edit forms
+    * Add location input field
+* Update campground routes (we do this by using the geocoder package: npm i -S node-geocoder)
+
+## Moment JS (v3 updates)
+In deployed v3 of this application we will be adding a "time since..." feature to our application, using Moment JS. The steps are as follows:
+* Install Moment JS
+* Require Moment and add it to app.locals
+* Update campground and comment models
+* Use moment in the show.ejs file
+
+## Admin User Role Authorization (v4 updates)
+In deployed v4 of this application we will be adding admin user functionality to our website. The steps are as follows:
+* Modify user model to include isAdmin object
+* Add admin code input to register.ejs
+* Modify index.js router.post() route to include admin check logic
+* Add logic inside of the campgrounds show.ejs to give admin rights to edit/delete posts
+* Additionally, modify middleware index.js ownership to include admin rights to edit/delete posts
+
+## User Profiles (v5 updates)
+In deployed v5 of this application we will be adding user profiles. The steps are as follows:
+* Update user model to include first name, last name, avatar, and email
+* Update register.ejs to reflect changes in user model
+* Add new instance of User() to index.js inside of the router.post() route for register
+* Add user profile route to index.js
+* Create a new view for user profiles at views/users/show.ejs
+* Modify user profile route in index.js to route post information to views/users/show.ejs
+
+## Edit User Profiles and UI Updates (v6 updates)
+In deployed v6 of this application we will be adding a view profile feature on the nav-bar as well as editing user profile and UI improvements. The steps are as follows:
+* Add associated user campgrounds to views/users/show.ejs
+* Add a dropdown feature to the "Signed in as..." tab
+* In the dropdown menu we will include a user profile link and an edit user profile link
+* Additionally, we will add an edit button to view/users/show.ejs which will only appear if the user owns the profile
+* Create a user.js file for routing purposes
+* Modify app.js to include user.js routes
+* Create edit.ejs under views/users/edit.ejs
+* Add RESTful routes to user.
+
+## Password Reset (v7 updates)
+In deployed v7 of this application we will be adding a password reset option for users. The steps are as follows:
+* Run *npm i -S async nodemailer* in the terminal (crypto is already part of express) 
+* Require async, nodemailer, and crypto into index.js route
+* Create a GET and POST route for the forgot password page
+* Ensure that a legitimate email and pass is passed into process.env.GMAILPW (export GMAILPW to .env) for the post route
+* Create a view for the forgot password page
+* Ensure that username and email is unique by updating the user model
+* Add a reset password token as well as reset password time-to-expiration limit to the user model
+* Create a GET and POST route for the reset password token page
+* As before, ensure that a legitimate email and pass is passed into process.env.GMAILPW (export GMAILPW to .env) for the post route
+* Create a view for the reset password token page 
+
+## Register Page Clean-up and Forgot Password Link + Change Password Page (v8 updates)
+* Fix error messages on register when email is empty and when email exists (custom messages in index.js route)
+* Update redirect for edit campgrounds page to redirect to campground show page
+* Remove admin entry form from register page (more secure, admin rights will be granted via mlab)
+* Add labels to register and login page for clarity and form separation (best practices)
+* Remove first name, last name, avatar, and bio from the sign-up page since users can populate during edits
+* Set default first name, last name, avatar, and bio when a new user is created (modify index.js route)
+* Modify campground ownership header on the user's profile page to display name and username
+* Add a link to the forgot password page on the login page (also needs styling)
+* Add new routes and a view for the change password page which can be accessed via navbar dropdown menu
---
+++ Deployed/v8/routes/index.js
@@ -0,0 +1,259 @@
+var express = require("express"),
+    router = express.Router(),
+    passport = require("passport"),
+    async = require("async"),
+    nodemailer = require("nodemailer"),
+    crypto = require("crypto"),
+    User = require("../models/user");
+
+// Render landing page as our root "/" page
+router.get("/", function(req, res) {
+    res.render("landing");
+});
+
+// ======================
+// AUTHENTICATION ROUTES
+// ======================
+
+// Register form
+router.get("/register", function(req, res) {
+    res.render("register", {page: "register"});
+});
+
+// Sign up logic
+router.post("/register", function(req, res) {
+    var newUser = new User({
+        username: req.body.username,
+        email: req.body.email,
+        firstName: "New",
+        lastName: "User",
+        avatar: "https://tinyurl.com/yaraujs7",
+        bio: "This user has not filled in any information"
+    });
+    
+    User.register(newUser, req.body.password, function(err, user, next) {
+        // Custom error message
+        if (err) {
+            if (err.message.indexOf("E11000") >= 0) {
+              err.message = "The given email is already registered";
+            }
+            if (err.message.indexOf("Path `email` is required") >= 0) {
+              err.message = "Please enter a valid email address";
+            }
+            // Log error
+            console.log(err);
+            return res.render("register", {error: err.message});
+        }
+        // Authenticate user
+        passport.authenticate("local")(req, res, function() {
+          // Flash then redirect to campgrounds
+          req.flash("success", "Welcome to YelpCamp, " + user.username + "!"); 
+          res.redirect("/campgrounds");
+        });
+    });
+});
+
+// Login form
+router.get("/login", function(req, res) {
+    res.render("login", {page: "login"});
+});
+
+// Login logic
+// Includes passport.authenticate() middleware
+router.post("/login", passport.authenticate("local", 
+    {
+        successRedirect: "/campgrounds",
+        failureRedirect: "/login",
+        successFlash: "Welcome back to YelpCamp!",
+        failureFlash: "Invalid username or password!"
+    }), function(req, res) {
+});
+
+// Logout route
+router.get("/logout", function(req, res) {
+    req.logout();
+    req.flash("success", "Logged you out!")
+    res.redirect("/campgrounds");
+});
+
+// ===================================
+// Password Forgot/Reset/Change Routes
+// ===================================
+
+// GET - Password forgot route
+router.get('/forgot', function(req, res) {
+    res.render('forgot');
+});
+
+// POST - Password forgot route
+router.post('/forgot', function(req, res, next) {
+  async.waterfall([
+    function(done) {
+      crypto.randomBytes(20, function(err, buf) {
+        var token = buf.toString('hex');
+        done(err, token);
+      });
+    },
+    function(token, done) {
+      User.findOne({ email: req.body.email }, function(err, user) {
+        if (!user) {
+          req.flash('error', 'No account with that email address exists.');
+          return res.redirect('/forgot');
+        }
+
+        user.resetPasswordToken = token;
+        user.resetPasswordExpires = Date.now() + 3600000; // 1 hour
+
+        user.save(function(err) {
+          done(err, token, user);
+        });
+      });
+    },
+    function(token, user, done) {
+      var smtpTransport = nodemailer.createTransport({
+        service: 'Gmail', 
+        auth: {
+          user: 'codewiththerealchefsteph@gmail.com',
+          pass: process.env.GMAILPW
+        }
+      });
+      var mailOptions = {
+        to: user.email,
+        from: 'codewiththerealchefsteph@gmail.com',
+        subject: 'Node.js Password Reset',
+        text: 'You are receiving this because you (or someone else) have requested the reset of the password for your account.\n\n' +
+          'Please click on the following link, or paste this into your browser to complete the process:\n\n' +
+          'http://' + req.headers.host + '/reset/' + token + '\n\n' +
+          'If you did not request this, please ignore this email and your password will remain unchanged.\n'
+      };
+      smtpTransport.sendMail(mailOptions, function(err) {
+        console.log('mail sent');
+        req.flash('success', 'An e-mail has been sent to ' + user.email + ' with further instructions.');
+        done(err, 'done');
+      });
+    }
+  ], function(err) {
+    if (err) return next(err);
+    res.redirect('/forgot');
+  });
+});
+
+// GET - Password reset route
+router.get('/reset/:token', function(req, res) {
+  User.findOne({ resetPasswordToken: req.params.token, resetPasswordExpires: { $gt: Date.now() } }, function(err, user) {
+    if (!user) {
+      req.flash('error', 'Password reset token is invalid or has expired.');
+      return res.redirect('/forgot');
+    }
+    res.render('reset', {token: req.params.token});
+  });
+});
+
+// POST - Password reset route
+router.post('/reset/:token', function(req, res) {
+  async.waterfall([
+    function(done) {
+      User.findOne({ resetPasswordToken: req.params.token, resetPasswordExpires: { $gt: Date.now() } }, function(err, user) {
+        if (!user) {
+          req.flash('error', 'Password reset token is invalid or has expired.');
+          return res.redirect('back');
+        }
+        if(req.body.password === req.body.confirm) {
+          user.setPassword(req.body.password, function(err) {
+            user.resetPasswordToken = undefined;
+            user.resetPasswordExpires = undefined;
+
+            user.save(function(err) {
+              req.logIn(user, function(err) {
+                done(err, user);
+              });
+            });
+          })
+        } else {
+            req.flash("error", "Passwords do not match.");
+            return res.redirect('back');
+        }
+      });
+    },
+    function(user, done) {
+      var smtpTransport = nodemailer.createTransport({
+        service: 'Gmail', 
+        auth: {
+          user: 'codewiththerealchefsteph@gmail.com',
+          pass: process.env.GMAILPW
+        }
+      });
+      var mailOptions = {
+        to: user.email,
+        from: 'codewiththerealchefsteph@gmail.com',
+        subject: 'Your password has been changed',
+        text: 'Hello,\n\n' +
+          'This is a confirmation that the password for your account ' + user.email + ' has just been changed.\n'
+      };
+      smtpTransport.sendMail(mailOptions, function(err) {
+        req.flash('success', 'Success! Your password has been changed.');
+        done(err);
+      });
+    }
+  ], function(err) {
+    res.redirect('/campgrounds');
+  });
+});
+
+// GET - Password change route
+router.get('/change', function(req, res) {
+  res.render("change", {page: "change"});
+});
+
+// POST - Password change route
+router.post('/change', function(req, res) {
+  async.waterfall([
+    function(done) {
+      User.findOne({ resetPasswordToken: req.params.token, resetPasswordExpires: { $gt: Date.now() } }, function(err, user) {
+        if (!user) {
+          req.flash('error', 'Password reset token is invalid or has expired.');
+          return res.redirect('back');
+        }
+        if(req.body.password === req.body.confirm) {
+          user.setPassword(req.body.password, function(err) {
+            user.resetPasswordToken = undefined;
+            user.resetPasswordExpires = undefined;
+
+            user.save(function(err) {
+              req.logIn(user, function(err) {
+                done(err, user);
+              });
+            });
+          })
+        } else {
+            req.flash("error", "Passwords do not match.");
+            return res.redirect('back');
+        }
+      });
+    },
+    function(user, done) {
+      var smtpTransport = nodemailer.createTransport({
+        service: 'Gmail', 
+        auth: {
+          user: 'codewiththerealchefsteph@gmail.com',
+          pass: process.env.GMAILPW
+        }
+      });
+      var mailOptions = {
+        to: user.email,
+        from: 'codewiththerealchefsteph@gmail.com',
+        subject: 'Your password has been changed',
+        text: 'Hello,\n\n' +
+          'This is a confirmation that the password for your account ' + user.email + ' has just been changed.\n'
+      };
+      smtpTransport.sendMail(mailOptions, function(err) {
+        req.flash('success', 'Success! Your password has been changed.');
+        done(err);
+      });
+    }
+  ], function(err) {
+    res.redirect('/campgrounds');
+  });
+});
+
+module.exports = router;
